6ce7855dd6e1449b6c048e3a8b9c759c398f3e8d
Zach Bloomquist <zbloomquist@fullambit.net>
2012-09-20 20:44:51 -0400

    Cleaned up and added error-checking to login dF

--- modules/users/users.dynamicForms.login.php
+++ modules/users/users.dynamicForms.login.php
@@ -1,7 +1,5 @@
 <?php
 function users_validateDynamicFormField($data,$db,$fieldItem,$fieldValue){
-	//var_dump($fieldItem);
-	//die();
 	if($fieldItem['name']=='Username'){
 		$fieldValues=array();
 		foreach($data->output['customForm']->fields as $field){
@@ -12,48 +10,15 @@ function users_validateDynamicFormField($data,$db,$fieldItem,$fieldValue){
 			':name' => $fieldValues['username']['value'],
 			':passphrase' => hash('sha256',$fieldValues['password']['value'])
 		));
-		var_dump($fieldValues);
 		if ($user=$statement->fetch(PDO::FETCH_ASSOC)) {
 			$data->user=$user;
-			// Set User TimeZone
-			if (!empty($user['timeZone']) && $user['timeZone']!==0) {
-				date_default_timezone_set($user['timeZone']);
-				ini_set('date.timezone', $user['timeZone']);
-			}
-			// Load permissions
-			getUserPermissions($db, $user);
-			// Purge existing sessions containing user ID
-			$statement=$db->prepare('purgeSessionByUserId');
-			$statement->execute(array(
-				'userId' => $user['id']
-			));
-			// Create new session
-			$userCookieValue = hash('sha256',
-				$user['id'].'|'.time().'|'.common_randomPassword(32, 64)
-			);
-			// Push expiration ahead
-			$statement=$db->query("userSessionTimeOut");
-			$data->settings['userSessionTimeOut'] = $statement->fetchColumn();
-			$expires=time()+$data->settings['userSessionTimeOut'];
-
-			if (isset($fieldValues['keep me logged in']) && $fieldValues['keep me logged in']=='on') {
-				$expires = time()+604800; // 1 week
-			}
-
-			// Update and sync cookie to server values
-			setcookie($db->sessionPrefix.'SESSID', $userCookieValue, $expires, $data->linkHome, '', '', true);
-			$expires=gmdate("Y-m-d H:i:s", $expires);
-			$statement=$db->prepare('updateUserSession');
-			$statement->execute(array(
-				':sessionId' => $userCookieValue,
-				':userId'    => $user['id'],
-				':expires'   => $expires,
-				':ipAddress' => $_SERVER['REMOTE_ADDR'],
-				':userAgent' => $_SERVER['HTTP_USER_AGENT']
-			));
 			return TRUE;
 		}else{
 			$data->output['customForm']->error=TRUE;
+			$data->output['customForm']->fields[$fieldValues['username']['name']]['error']=TRUE;
+			$data->output['customForm']->fields[$fieldValues['username']['name']]['errorList'][]='Invalid username or password.';
+			$data->output['customForm']->fields[$fieldValues['password']['name']]['error']=TRUE;
+			$data->output['customForm']->fields[$fieldValues['password']['name']]['errorList'][]='Invalid username or password.';
 			return FALSE;
 		}
 	}
@@ -61,4 +26,42 @@ function users_validateDynamicFormField($data,$db,$fieldItem,$fieldValue){
 function users_saveDynamicFormField($data,$db,$fieldName,$fieldValue){
 }
 function users_afterForm($data,$db){
+	if(!empty($data->user)){
+		$user=$data->user;
+		// Set User TimeZone
+		if (!empty($user['timeZone']) && $user['timeZone']!==0) {
+			date_default_timezone_set($user['timeZone']);
+			ini_set('date.timezone', $user['timeZone']);
+		}
+		// Load permissions
+		getUserPermissions($db, $user);
+		// Purge existing sessions containing user ID
+		$statement=$db->prepare('purgeSessionByUserId');
+		$statement->execute(array(
+			'userId' => $user['id']
+		));
+		// Create new session
+		$userCookieValue = hash('sha256',
+			$user['id'].'|'.time().'|'.common_randomPassword(32, 64)
+		);
+		// Push expiration ahead
+		$statement=$db->query("userSessionTimeOut");
+		$data->settings['userSessionTimeOut'] = $statement->fetchColumn();
+		$expires=time()+$data->settings['userSessionTimeOut'];
+		if(isset($fieldValues['keep me logged in'])&&$fieldValues['keep me logged in']=='on'){
+			$expires = time()+604800; // 1 week
+		}
+		// Update and sync cookie to server values
+		setcookie($db->sessionPrefix.'SESSID', $userCookieValue, $expires, $data->linkHome, '', '', true);
+		$expires=gmdate("Y-m-d H:i:s", $expires);
+		$statement=$db->prepare('updateUserSession');
+		$statement->execute(array(
+			':sessionId' => $userCookieValue,
+			':userId'    => $user['id'],
+			':expires'   => $expires,
+			':ipAddress' => $_SERVER['REMOTE_ADDR'],
+			':userAgent' => $_SERVER['HTTP_USER_AGENT']
+		));
+		return TRUE;
+	}
 }
